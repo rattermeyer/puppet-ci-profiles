00e957da51b1c9c14f826a561749fa1d30c684e2
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-07-10 19:37:01 +0000

    sonar variables added

--- manifests/sonar.pp
+++ manifests/sonar.pp
@@ -1,25 +1,32 @@
-class profiles::sonar() {
-  mysql::db { 'sonar':
-      user     => 'sonar',
-      password => 'sonar',
+class profiles::sonar(
+  $sonar_user = 'sonar',
+  $sonar_group = 'sonar',
+  $sonar_db = 'sonar',
+  $sonar_db_user = 'sonar',
+  $sonar_db_password = 'sonar',
+  $sonar_jdbc = {
+    url               => "jdbc:mysql://${ipaddress_eth0}:3306/sonar",
+    username          => "${sonar_db_user}",
+    password          => "${sonar_db_password}",
+  },
+  $sonar_version = '4.3.2'
+) {
+  mysql::db { "${sonar_db}":
+      user     => $sonar_db_user,
+      password => $sonar_db_password,
       host     => '%.%.%.%',
       grant    => ['ALL'],
   }
-  mysql_grant { 'sonar@%.%.%.%': 
+  mysql_grant { "${sonar_db_user}@%.%.%.%": 
     ensure => 'present',
     privileges => ['ALL'],
-    table      => 'sonar.*',
-    user       => 'sonar@%.%.%.%',
-  }
-  $sonar_jdbc = {
-    url               => "jdbc:mysql://${ipaddress_eth0}:3306/sonar",
-    username          => 'sonar',
-    password          => 'sonar',
+    table      => "${sonar_db_user}.*",
+    user       => "${sonar_db_user}@%.%.%.%",
   }
   class { 'sonarqube' :
-    version      => '4.3.2',
-    user         => 'sonar',
-    group        => 'sonar',
+    version      => $sonar_version,
+    user         => $sonar_user,
+    group        => $sonar_group,
     service      => 'sonar',
     installroot  => '/usr/local',
     home         => '/var/local/sonar',
@@ -30,5 +37,5 @@ class profiles::sonar() {
     context_path => '/sonar',
     require  => Class['maven::maven'],
   }
-  create_resources(sonarqube::plugin, hiera_hash('profile::sonar::plugins'), {'groupid' => 'org.codehaus.sonar-plugins', 'require' => Class['maven::maven']})
+  create_resources(sonarqube::plugin, hiera_hash('profile::sonar::plugins'), {'require' => Class['maven::maven']})
 }
